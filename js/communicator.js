d5bf2171b9a270eba434fd4e4147bfbcf8bb149c
Darshan Raj <darshan.rajd@gmail.com>
2014-07-10 10:22:24 +0530

    Update communicator.js
    Updated token

--- js/communicator.js
+++ js/communicator.js
@@ -1,6 +1,6 @@
 // the module that communcates with the graph api of facebook using jsonp
 FBS.Communicator = function(){
-	var token = "CAACEdEose0cBAEFhxQJdfJQLJjj3Hd06Xk4REAAHDCORMwFeTbakPxK1tWrflVkXEKtBZAwtuFhAhM7B2ZBmbkSk0ZB0vfaoCVaJJg5751xdg5awPYZCMYquka0tcIZCRK2JH6bCEznitZAjeMiJlLLv9aEt0xZBgcv3u5bODAO0sizKeUzSxNSqSqQwAmdSftlRtAvWNEWFQZDZD";
+	var token = "CAACEdEose0cBAHotQtc1QS6ZB2YN7Niyl8AfWGcfoSKS11u25EOUvYbardvbxBE4yZB8APuvq1AWtT9BgCLQZBN2jk27PGA5B2ZA5trBa8SKtSghFyuMScKDnFGfyE612f1wAHPfC36F9aNeDNVYHjrI2XbDjpfYrbuQyO1M4qmT4B0gwfGi2fOI6tntBV8ZD";
 	var appCallback;		// the callback of the app to be called after receiving the jsonp response
 
 	var fireRequest = function(query){
